c3e45ed1989aa55d50957f347aae3a70e916a7e6
CJavatX <cdpm98@hotmail.com>
2024-02-14 11:28:19 -0600

    SECCIÓN 3 - TERMINADO

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -2,7 +2,8 @@ import './style.css'
 // import { name, age } from './bases/01-types'
 // import { pokemon } from './bases/02-objects'
 // import { charmander } from './bases/04-injection'
-import { charmander } from './bases/05-decorators'
+// import { charmander } from './bases/05-decorators'
+import { charmander } from './bases/06-decorators2'
 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
