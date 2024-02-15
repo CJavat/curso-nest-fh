01fa93415e10451946f804b29f198dcc6516b3db
CJavatX <cdpm98@hotmail.com>
2024-02-15 12:59:09 -0600

    SECCIÓN 7 - TERMINADO

--- 03-pokedex/src/main.ts
+++ 03-pokedex/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v2'); // Funciona para poner un prefijo global en la aplicación. Ejemplo: /'api'/las-demas-rutas
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  )
+
   await app.listen(3000);
 }
 bootstrap();
---
+++ 03-pokedex/src/pokemon/pokemon.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { PokemonService } from './pokemon.service';
+import { PokemonController } from './pokemon.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
+
+@Module({
+  controllers: [PokemonController],
+  providers: [PokemonService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Pokemon.name, // Nombre de la clase que está en el Entity.
+        schema: PokemonSchema, // Nombre del esquema que estamos exportando en la clase del Entity.
+      }
+    ]),
+  ]
+})
+export class PokemonModule {}
