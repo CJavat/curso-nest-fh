7edf225d43111253fd8a85d20e8a06a9f7adae88
CJavatX <cdpm98@hotmail.com>
2024-02-16 16:09:09 -0600

    SECCIÓN 9 - TERMINADO

--- 03-pokedex/README.md
+++ 03-pokedex/README.md
@@ -4,20 +4,30 @@
 
 # Ejecutar en desarrollo
 
-1. Clonar el repositorio
-2. Ejecutar
+1. Clonar el repositorio.
+2. Ejecutar:
 ```
 npm install
 ```
-3. Tener Nest CLI instalado
+3. Tener Nest CLI instalado:
 ```
 npm i -g @nest/cli
 ```
-4. Levantar la base de datos
+4. Levantar la base de datos:
 ```
 docker-compose up -d
 ```
-5. Reconstruir la base de datos con la semilla
+
+5. Clonar el archivo __.env.template__ y renombrar la copia a __.env__.
+
+6. Llenar las variables de entorno definidas en el ```.env```.
+
+7. Ejecutar la aplicación de dev:
+```
+npm run start:dev
+```
+
+8. Reconstruir la base de datos con la semilla:
 ```
 http://localhost:3000/api/v2/seed
 ```
@@ -25,3 +35,11 @@ http://localhost:3000/api/v2/seed
 ## Stack Usado
 * MongoDB
 * Nest
+
+# Production build
+1. Crear el archivo ```.env.prod```
+2. Llenar las variables de entorno de producción
+3. Crear la nueva imagen 
+```
+docker-compose -f docker-compose.prod.yaml --env-file .env.prod up --build
+```
--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -5,13 +5,22 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/app.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [ EnvConfiguration ],
+      validationSchema: JoiValidationSchema
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'), // Para agregar el mongoose al proyecto, se le pone la url y la base de datos.
+    MongooseModule.forRoot( process.env.MONGODB, {
+      dbName: 'pokemonsdb'
+    } ), // Para agregar el mongoose al proyecto, se le pone la url y la base de datos.
     PokemonModule, CommonModule, SeedModule,
   ],
 })
